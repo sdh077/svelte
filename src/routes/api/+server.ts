72d24b6b8c8c31f94a536eba7c2c357cc93e21f5
shindaeho <sdh077@naver.com>
2023-06-16 17:23:40 +0900

    vercel - init

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,27 +1,31 @@
 import { json } from '@sveltejs/kit';
 
 export const GET = async () => {
-    const posts = await fetchMarkdownPosts();
-    return json(posts);
+	const posts = await fetchMarkdownPosts();
+	return json(posts);
 };
 
 const fetchMarkdownPosts = async () => {
-    const contentFiles = import.meta.glob('/src/routes/content/*/+page.svelte');
-    const startFiles = import.meta.glob('/src/routes/start/*/+page.svelte');
+	const contentFiles = import.meta.glob('/src/routes/content/*/+page.svelte');
+	const startFiles = import.meta.glob('/src/routes/start/*/+page.svelte');
+	const vercelFiles = import.meta.glob('/src/routes/vercel/*/+page.svelte');
 
-    const iterContents = Object.entries(contentFiles);
-    const iterStarts = Object.entries(startFiles);
+	const iterContents = Object.entries(contentFiles);
+	const iterStarts = Object.entries(startFiles);
+	const iterVercels = Object.entries(vercelFiles);
 
-    const content = await iterPromise(iterContents, 20);
-    const start = await iterPromise(iterStarts, 18);
+	const content = await iterPromise(iterContents, 20);
+	const start = await iterPromise(iterStarts, 18);
+	const vercel = await iterPromise(iterVercels, 19);
 
-    return { start, content }
+	return { start, content, vercel };
 };
 
-const iterPromise = async (iter: [string, () => Promise<unknown>][], s: number) => await Promise.all(
-    iter.map(async ([path, resolver]) => {
-        const { metadata } = await resolver();
-        const filePath = path.slice(s, -13)
-        return filePath;
-    })
-);
+const iterPromise = async (iter: [string, () => Promise<unknown>][], s: number) =>
+	await Promise.all(
+		iter.map(async ([path, resolver]) => {
+			const { metadata } = await resolver();
+			const filePath = path.slice(s, -13);
+			return filePath;
+		})
+	);
---
+++ src/routes/vercel/init/+page.svelte
@@ -0,0 +1,12 @@
+<h1>vercel에 프로젝트 등록</h1>
+<div>
+	<h5>
+		1. 버셀 접속 후 가입 >
+		<button type="button" class="btn bg-gray-300">
+			<a href="https://vercel.com/" target="_blank">https://vercel.com/</a>
+		</button>
+	</h5>
+</div>
+<div>
+	<h5>2. 대시보드에서 Add New를 클릭하여 Git Repo 선택 또는 Clone Template을 통해 프로젝트 생성</h5>
+</div>
