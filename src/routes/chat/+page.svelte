2539dd7feecb23c21a485f8fa3f6d97319a422a6
shindaeho <sdh077@naver.com>
2023-07-22 20:23:40 +0900

    room 추가

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,64 +1,12 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { io } from 'socket.io-client';
-	const socket = io('http://15.165.12.69:3000/', {});
-	// const socket = io('http://localhost:3000', {});
-	let textfield = '';
-	let username = '';
-	let chat;
-	let messages = [];
-
-	onMount(async () => {
-		socket.on('chat message', (message) => {
-			messages = [...messages, message];
-			setTimeout(() => {
-				chat.scrollTop = chat.scrollHeight + 100;
-			}, 10);
-		});
-	});
-
-	function sendMessage() {
-		const message = textfield.trim();
-		if (!message) return;
-
-		textfield = '';
-		socket.emit('chat message', message);
-	}
+	const rooms = [
+		{
+			id: 'qwe12',
+			title: '친구랑'
+		}
+	];
 </script>
 
-<div class="h-screen w-screen bg-zinc-800">
-	<div class="h-full w-full max-w-md mx-auto bg-zinc-500 flex flex-col">
-		<header
-			class="px-6 py-4 border-b border-zinc-800 bg-zinc-700 text-white shrink-0 flex items-center justify-between"
-		>
-			<span class="font-bold text-xl">My Chat app</span>
-			<span>{username}</span>
-		</header>
-
-		<div class="h-full w-full p-4 overflow-y-auto snap-x" bind:this={chat}>
-			{#each messages as message}
-				<div class="bg-zinc-300 rounded-xl rounded-tl-none px-4 py-3 my-4 w-fit">
-					<span class="flex items-center space-between gap-4">
-						<b>{message.from}</b>
-						<i>{message.time}</i>
-					</span>
-					{message}
-				</div>
-			{/each}
-		</div>
-
-		<form
-			action="#"
-			on:submit|preventDefault={sendMessage}
-			class="px-6 py-4 border-t border-zinc-800 bg-zinc-700 text-white shrink-0 flex items-center"
-		>
-			<input
-				type="text"
-				bind:value={textfield}
-				placeholder="Type something..."
-				class="bg-transparent border-none px-4 py-3 w-full"
-			/>
-			<button type="submit" class="shrink-0 border border-white rounded-lg px-4 py-3">Send</button>
-		</form>
-	</div>
-</div>
+{#each rooms as room}
+	<div>채팅방 {room.title} <a href="/chat/room/{room.id}"><button>입장</button></a></div>
+{/each}
---
+++ src/routes/chat/room/[id]/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { onMount, beforeUpdate, afterUpdate } from 'svelte';
+	import { page } from '$app/stores';
+
+	import { io } from 'socket.io-client';
+	// const socket = io('ws://15.165.12.69:3000/', {});
+	const socket = io(`ws://localhost:3000/room?id=${$page.params.id}`, {});
+	let user = '이름입력';
+	let textfield = '';
+	let chat;
+	let messages = [];
+	onMount(() => {
+		socket.on('reply', (message) => {
+			messages = [...messages, message];
+			setTimeout(() => {
+				if (chat) chat.scrollTop = chat?.scrollHeight ?? 0 + 100;
+			}, 50);
+		});
+	});
+	let autoscroll = false;
+
+	beforeUpdate(() => {
+		if (chat) {
+			const scrollableDistance = chat.scrollHeight - chat.offsetHeight;
+			autoscroll = chat.scrollTop > scrollableDistance - 20;
+		}
+	});
+	afterUpdate(() => {
+		// if (autoscroll) chat.scrollTo(0, chat.scrollHeight);
+	});
+
+	async function sendMessage() {
+		if (textfield) {
+			const message = textfield.trim();
+			textfield = '';
+			if (!message) return;
+			socket.emit('chat message', { type: 2, user, message });
+		}
+	}
+</script>
+
+<input type="text" bind:value={user} />
+<div
+	class="container overflow-y-auto h-full"
+	style="height: 50vh; scroll-behavior: smooth;"
+	bind:this={chat}
+>
+	{#each messages as message}
+		{#if message.type === 1}
+			<div>{message.message}</div>
+		{:else}
+			<div>{message.user} : {message.message}</div>
+		{/if}
+	{/each}
+</div>
+
+<form
+	action="#"
+	on:submit|preventDefault={sendMessage}
+	class="px-6 py-4 border-t border-zinc-800 bg-zinc-700 text-white shrink-0 flex items-center"
+>
+	<input
+		type="text"
+		bind:value={textfield}
+		placeholder="Type something..."
+		class="bg-transparent border-none px-4 py-3 w-full"
+	/>
+	<button type="submit" class="shrink-0 border border-white rounded-lg px-4 py-3">Send</button>
+</form>
